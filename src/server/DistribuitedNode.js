8ff7f7a000536321a1e116c9afda4f7aa1a952df
Gabriel Santos <59262903+GabrielFSSantos@users.noreply.github.com>
2024-01-11 17:35:36 -0300

    Join

--- src/server/DistribuitedNode.js
+++ src/server/DistribuitedNode.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const socketIo = require("socket.io");
+const { Pool } = require("pg");
 
 const printEnvironmentVariables = require("./utils/PrintEnvironmentVariables");
 const ipsToObjectSorted = require("./utils/IpsToObjectSorted");
 const connecToNode = require("./utils/ConnecToNode");
 const getClientIp = require("./utils/GetClientIp");
 const getClientPort = require("./utils/GetClientPort");
+
+const MIN_INTERVAL = 5000; // 5 segundos
+const MAX_INTERVAL = 15000; // 15 segundos
+const TIMEOUT_LIMIT = 7000; // 7 segundos para timeout
 
 class DistributedNode {
   constructor() {
@@ -23,6 +28,10 @@ class DistributedNode {
 
     this.InElection = false;
     this.requestSent = false;
+
+    this.requestQueue = []; // Fila de requisições
+    this.queueLimit = 10; // Limite máximo da fila
+    this.isProcessing = false; // Flag para indicar se uma requisição está sendo processada
   }
 
   async initServer() {
@@ -61,28 +70,22 @@ class DistributedNode {
         
         printEnvironmentVariables(this);
       });
-
-      // Evento - Requisição em processamento
-      socket.on("RequestInProcessing", (data) => {
-        this.requestSent = true;
-      });
-
-      // Evento - Coordenador - resposta de requisição GET
-      socket.on("RequestGet", async (data) => {
-        if(this.isCoordinator) {
-          const clientIp = getClientIp(socket);
-          const clientPort = getClientPort(clientIp);
-          const clientSocket = await connecToNode(`${clientIp}:${clientPort}`);
-          clientSocket.emit('RequestInProcessing', { mensagem: 'Response Teste 1' });
-          //Entra na Fila
-        }
-      });
     });
     
 
     // SIMULAÇÃO
     await this.startElection([]);
-    
+
+    setTimeout(async() => {
+      if(this.isCoordinator) {
+        await this.setupCoordinatorServer();
+      }
+      else {
+        await this.setupRegularNodeServer();
+      }
+    }, 20000);
+
+    /*
     setTimeout(async() => {
       if(this.isCoordinator) {
         console.log("Teste Desconexão")
@@ -109,21 +112,8 @@ class DistributedNode {
       }
     }, 80000);
 
-  }
+    */
 
-  // Verifica conexão com nó sucessor 
-  async verifySuccessor() {
-    if(this.successorIp) {
-      const successorPort = getClientPort(this.successorIp);
-      let successorSocket = await connecToNode(`${this.successorIp}:${successorPort}`);
-  
-      if(successorSocket && successorSocket.connected) {
-        return successorSocket;
-      }
-    }
-    else {
-      return await this.electSuccessor();
-    }
   }
 
   // Conecta com nó sucessor
@@ -236,31 +226,170 @@ class DistributedNode {
   }
 
   // Exemplo de requisição ao Coordenador
-  async requestExemple() {
+  async setupRegularNodeServer() {
 
     let coordinatorPort = getClientPort(this.coordinatorIp);
     let coordinatorSocket = await connecToNode(`${this.coordinatorIp}:${coordinatorPort}`);
 
     if(coordinatorSocket && coordinatorSocket.connected) {
-      coordinatorSocket.emit('RequestGet', { mensagem: 'Request Teste 1' });
-      this.requestSent = false;
-
-      setTimeout(async() => {
-        if(this.requestSent) {
-          console.log(`Requisição em processamento!`);
-        }
-        else{
-          console.log(`Requisição sem resposta!`);
-          await this.removeCoordinator();
-          await this.startElection([]);
-        }
-      }, 5000);
+      
+      this.coordinatorSocket.on("connect", () => {
+        console.log(`Conectado ao coordenador no endereço ${this.coordinatorIp}:${coordinatorPort}`);
+        this.initiateRandomRequests(coordinatorSocket);
+      });
     }
-    else{
-      await this.startElection([]);
-    }
-    
   }
+
+  // Configuração e conexão com banco
+  async connectToDatabase() {
+    // Configuração do banco de dados
+    const dbConfig = {
+      host: "172.25.0.2",
+      port: 5432,
+      user: "user",
+      password: "password",
+      database: "distributed_systems_db",
+    };
+
+    this.dbPool = new Pool(dbConfig);
+
+    await this.dbPool.connect((error) => {
+      if (error) {
+        console.error("Erro ao conectar no banco de dados:", error);
+        return;
+      }
+
+      console.log("Conectado ao banco de dados PostgreSQL com sucesso.");
+    });
+  }
+
+  async disconnectFromDatabase() {
+    // Verifica se a pool de conexões existe e está conectada
+    if (this.dbPool) {
+      await this.dbPool
+        .end()
+        .then(() =>
+          console.log("Desconectado do banco de dados PostgreSQL com sucesso.")
+        )
+        .catch((error) =>
+          console.error("Erro ao desconectar do banco de dados:", error)
+        );
+    }
+  }
+
+  // Configurar servidor do coordenador
+  async setupCoordinatorServer() {
+    await this.connectToDatabase();
+
+    this.io.on("connection", (socket) => {
+      console.log("Nó conectado:", socket.id);
+
+      socket.on("log_request", (requestData) => {
+        console.log("Recebida solicitação de log:", requestData);
+        this.addToQueue(requestData, socket);
+      });
+    });
+  }
+
+  // Inicia um ciclo de solicitações aleatórias ao coordenador
+  async initiateRandomRequests(coordinatorSocket) {
+    const sendRequest = () => {
+      const requestData = {
+        type: "log_request",
+        hostname: this.hostname,
+        timestamp: Date.now(),
+        requestId: `req-${Date.now()}-${Math.random()}`,
+      };
+
+      const timeout = setTimeout(async () => {
+        console.log(
+          `Tempo de resposta excedido para ${requestData.requestId}.`
+        );
+        coordinatorSocket.off(`log_response-${requestData.requestId}`);
+
+        // Desconectar do coordenador
+        await this.removeCoordinator();
+
+        // Iniciar o processo de eleição
+        await this.startElection([]);
+      }, TIMEOUT_LIMIT);
+
+      coordinatorSocket.once(
+        `log_response-${requestData.requestId}`,
+        (response) => {
+          clearTimeout(timeout);
+          console.log("Resposta recebida do coordenador:", response);
+          coordinatorSocket.off(`log_response-${requestData.requestId}`);
+        }
+      );
+
+      coordinatorSocket.emit("log_request", requestData);
+    };
+
+    setInterval(() => {
+      const randomDelay = Math.random() * MAX_INTERVAL;
+      setTimeout(sendRequest, randomDelay);
+    }, MIN_INTERVAL);
+  }
+
+  // Adiciona na fila
+  addToQueue(requestData, socket) {
+    if (this.requestQueue.length < this.queueLimit) {
+      this.requestQueue.push({ requestData, socket });
+      console.log("Requisição adicionada à fila:", requestData);
+    } else {
+      // Opção de tratamento quando a fila está cheia
+      console.log("Tamanho máximo da fila atingido. ");
+    }
+    if (!this.isProcessing) {
+      this.processNextInQueue();
+    }
+  }
+
+  // Processa o próximo item na fila
+  processNextInQueue() {
+    if (this.requestQueue.length > 0) {
+      this.isProcessing = true;
+      const { requestData, socket } = this.requestQueue.shift();
+      this.processRequest(requestData, socket);
+    } else {
+      this.isProcessing = false;
+    }
+  }
+
+  // Processa uma requisição específica
+  processRequest(request, socket) {
+    console.log("Processando requisição:", request);
+
+    const queryText =
+      "INSERT INTO log_entries(hostname, timestamp) VALUES($1, $2)";
+    const values = [request.hostname, new Date(request.timestamp)];
+
+    this.dbPool.query(queryText, values, (err, res) => {
+      if (err) {
+        console.error("Erro ao gravar no banco de dados:", err.stack);
+        // Envia a resposta através do socket especificado
+        socket.emit(`log_response-${request.requestId}`, {
+          status: "Failure",
+          error: err.message,
+        });
+      } else {
+        console.log("Gravação no banco de dados bem-sucedida:", res.rows[0]);
+        // Envia a resposta através do socket especificado
+        socket.emit(`log_response-${request.requestId}`, {
+          status: "Success",
+          data: res.rows[0],
+        });
+      }
+
+      // Após processar, processar a próxima requisição na fila
+      setTimeout(() => {
+        this.isProcessing = false;
+        this.processNextInQueue();
+      }, 5000); // Delay para simular o processamento e garantir exclusão mútua
+    });
+  }
+
 }
 
 module.exports = DistributedNode;
